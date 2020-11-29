85d2087a9fb92feae524106dcc24842725cc23d9
Jakub Caban <kuba@whyblack.pl>
2020-11-29 12:24:57 +0100

    Add Redis via docker

--- tests/Benchmark/CachedLocalBench.php
+++ tests/Benchmark/CachedLocalBench.php
@@ -6,13 +6,18 @@ use League\Flysystem\FilesystemAdapter;
 use League\Flysystem\Local\LocalFilesystemAdapter;
 use Lustmored\Flysystem\Cache\CacheAdapter;
 use function PHPUnit\Framework\fileExists;
-use Symfony\Component\Cache\Adapter\ArrayAdapter;
+use Redis;
+use Symfony\Component\Cache\Adapter\RedisAdapter;
 
 class CachedLocalBench extends AbstractFilesystemBenchmark
 {
     protected static function createFilesystemAdapter(): FilesystemAdapter
     {
-        $pool = new ArrayAdapter();
+        $redis = new Redis();
+        $redis->connect('127.0.0.1');
+        $pool = new RedisAdapter(
+            $redis
+        );
         $dir = dirname(__DIR__).'/files/';
         if (!fileExists($dir)) {
             mkdir($dir);
--- tests/Benchmark/CachedMemoryBench.php
+++ tests/Benchmark/CachedMemoryBench.php
@@ -5,13 +5,18 @@ namespace Tests\Lustmored\Flysystem\Cache\Benchmark;
 use League\Flysystem\FilesystemAdapter;
 use League\Flysystem\InMemory\InMemoryFilesystemAdapter;
 use Lustmored\Flysystem\Cache\CacheAdapter;
-use Symfony\Component\Cache\Adapter\ArrayAdapter;
+use Redis;
+use Symfony\Component\Cache\Adapter\RedisAdapter;
 
 class CachedMemoryBench extends AbstractFilesystemBenchmark
 {
     protected static function createFilesystemAdapter(): FilesystemAdapter
     {
-        $pool = new ArrayAdapter();
+        $redis = new Redis();
+        $redis->connect('127.0.0.1');
+        $pool = new RedisAdapter(
+            $redis
+        );
         $adapter = new InMemoryFilesystemAdapter();
 
         return new CacheAdapter($adapter, $pool);
